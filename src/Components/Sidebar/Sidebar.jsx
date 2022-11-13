984ae3ab9c2813463a654c4bc4d92adc98a92b0b
suleymankoramaz <suleymankoramaz01@gmail.com>
2022-11-13 19:33:44 +0300

    Added logo to Sidebar
    Added shelf app logo to Sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Logo from '../../imgs/logo.png'
 import './Sidebar.css'
 
 function Sidebar() {
   return (
     <div className="Sidebar">
-        
+        <div className="logo">
+            <img src={Logo} alt="" />              
+        </div>
     </div>
   );
 };
