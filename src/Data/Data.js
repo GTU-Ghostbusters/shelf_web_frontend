01952a334e1370b61ff4ae471fd6aba129597c8a
suleymankoramaz <suleymankoramaz01@gmail.com>
2022-11-13 19:27:44 +0300

    Created SidebarData in Data.js
    SidebarData is a array includes icons that imported from react-unicons.

---
+++ src/Data/Data.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import {
+    UilEstate,
+    UilClipboardAlt,
+    UilUsersAlt,
+    UilPackage,
+    UilChart,
+    UilSignOutAlt,
+  } from "@iconscout/react-unicons";
+
+  export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Go account page",
+  },
+  {
+    icon: UilEstate,
+    heading: "Go home page",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Add book",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Remove book",
+  },{
+    icon: UilClipboardAlt,
+    heading: "List all books",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Add member",
+  },{
+    icon: UilUsersAlt,
+    heading: "Remove member",
+  },{
+    icon: UilUsersAlt,
+    heading: "Edit member",
+  },
+  {
+    icon: UilSignOutAlt,
+    heading: 'Exit'
+  },
+];
