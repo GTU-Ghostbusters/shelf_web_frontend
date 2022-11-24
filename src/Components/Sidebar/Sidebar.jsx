a3bc76bc33b0f3fee6c2db2050c530636544f7a3
Süleyman Koramaz <suleymankoramaz01@gmail.com>
2022-11-25 02:04:26 +0300

    Finished Sidebar
    Finished Sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
+import { useState } from 'react';
+import { SidebarData } from '../../Data/Data';
 import Logo from '../../imgs/logo.png'
 import './Sidebar.css'
 
+
 function Sidebar() {
-  return (
-    <div className="Sidebar">
-        <div className="logo">
-            <img src={Logo} alt="" />              
+
+    const [selected , setSelected] = useState(0);
+    return (
+        <div className="Sidebar">
+            <div className="logo">
+                <img src={Logo} alt="" />              
+            </div>
+
+            <div classhName="menu">
+                {SidebarData.map((item , index) => {
+                    return(
+                        <div className={selected===index?'menuItem active':'menuItem'}
+                            key={index}
+                            onClick={()=>setSelected(index)}
+                        >
+                            <item.icon/>
+                            <span>
+                                {item.heading}
+                            </span>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Sidebar
