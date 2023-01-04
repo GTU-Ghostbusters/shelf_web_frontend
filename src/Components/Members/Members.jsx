474154fb841a776ff7f3caf2c943d6d19091b391
Süleyman Koramaz <suleymankoramaz01@gmail.com>
2023-01-04 23:45:54 +0300

    Created Pagination
    Created Pagination for User page

--- src/Components/Members/Members.jsx
+++ src/Components/Members/Members.jsx
@@ -1,70 +1,129 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 import { useState } from 'react'
 import './Members.css'
-import axios from "axios";
+import axios from "axios"
 
 const Members = () => {
 
     const [users , setUsers] = useState([]);
-    const classes = {
-        'container':true,
-        "MermberList":true,
-    };
-
     const [userInfo , setUserInfo] = useState([]);
     const [userInfoVis , setUserInfoVis] = useState(0);
-    
-    useEffect( () => {
-        axios
-        .get('users.json')
-        .then((res) =>
-        setUsers(res.data));
-    },[])
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [usersPerPage] = useState(10);
+    const [query, setQuery] = useState(""); 
+    const classes = {
+        'container':true,
+        "BookList":true,
+    };
 
+    useEffect( () => {
+        setLoading(1);
+        axios
+        .get('https://hodikids.com/api/users')
+        .then((res) =>
+        setUsers(res.data.filter(isAdmin)));
+        setLoading(0);
+    },[userInfoVis])
+    
+    function isAdmin(item) {
+        if (item.is_superuser === 1) {
+            return false;
+        }
+        return true;
+    }
+    function search(data) {
+        return users.filter(
+          (item) =>
+            search_parameters.some((parameter) =>
+              item[parameter].toString().toLowerCase().includes(query)
+            )
+        );
+      }
+
+    const indexOfLastUser = currentPage * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+    const pageNumbers = [];
+    const search_parameters = Object.keys(Object.assign({}, ...currentUsers));
+
+    for (let i = 1; i <= Math.ceil(users.length / usersPerPage); i++) {
+        pageNumbers.push(i);
+    }
     return (
         <div>
-            <div className="Members">
+            <div  className="Members">
                 {(() => {
                     switch (userInfoVis) {
                     case 0:
-                        return <ul className={classes} >
-                                    {users.map((user) => (
-                                        <li className="MembersListElement" key={user.id}
-                                            onClick = { () =>
-                                            {
-                                                setUserInfo(user)
-                                                setUserInfoVis(1)
-                                            }
-                                        }>
-                                            {user.username}
-                                        </li>
+                        if (loading) {
+                            return <h2>Loading...</h2>;
+                        }
+                        return  <div className={classes}>
+                                    <label htmlFor="search-form">
+                                        <input
+                                            type="search"
+                                            name="search-form"
+                                            id="search-form"
+                                            className="search-input"
+                                            placeholder="Search user..."
+                                            onChange={(e) => setQuery(e.target.value)}
+                                        />
+                                    </label>
+                                    {search(currentUsers).map((user) => (
+                                        <li className="MemberList" key={user.id} 
+                                        onClick = { () =>
+                                           {   
+                                               setUserInfo(user)
+                                               setUserInfoVis(1)
+                                           }
+                                           }>
+                                       {user.email}
+                                       </li>
                                     ))}
-                                </ul>  
+                                    <nav className="page">
+                                        {pageNumbers.map(number => (
+                                          <button key={number} className='page-item'>
+                                            <a onClick={() => paginate(number)} href='!#'>
+                                              {number}
+                                            </a>
+                                          </button>
+                                        ))}
+                                    </nav>
+                                </div>  
                     case 1:
                         return  <div className="userInfo">
                                     <button className="buttonVis"
                                         onClick={ () => setUserInfoVis(0)}>EXIT</button>
                                     <pre>
-                                        User name: 
-                                        {userInfo.username}
-                                    </pre>
-                                    <pre>
-                                        First name: 
-                                        {userInfo.first_name}
-                                    </pre>
-                                    <pre>
-                                        Last name:
-                                        {userInfo.last_name}
+                                        Name: 
+                                        {userInfo.name}
                                     </pre>
                                     <pre>
                                         E-mail:
                                         {userInfo.email}
                                     </pre>
-                                    <pre>
-                                        Phone number:
-                                        {userInfo.phone}
-                                    </pre>
+                                    <button onClick={() => 
+                                    {
+                                        setUserInfoVis(2)
+                                    }}>
+                                        delete
+                                    </button>
+                                </div>
+                    case 2:
+                        return  <div className="popup">
+                                    The user with that email: <b>{userInfo.email}</b> will be blocked. Are you sure?
+                                    <button onClick={ () => {
+                                        axios
+                                        .get(`https://hodikids.com/api/user/${userInfo.id}/delete`)
+                                        setUserInfoVis(0) }}
+                                        > Yes
+                                    </button>
+                                    <button onClick={ () => {setUserInfoVis(1)}}>
+                                        No
+                                    </button>
                                 </div>
                     default:
                         return null
